afca00022802743c2c74401f717a5e0bf3cd0361
GoodGD <dionatha.work@gmail.com>
2025-02-06 11:02:48 -0300

    project setup frontend

--- backend/heroes/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Hero  # Importa o modelo Hero
-
-@admin.register(Hero)
-class HeroAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'image')  # Exibe essas colunas na lista de heróis
